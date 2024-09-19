539a02ec87ca1b5dbdff1dbd7b5b865c18c6b50b
Pawan Samdani <pawan@eumentis.com>
2024-09-19 15:01:17 +0530

    Empty pipeline

--- bin/testing-cdk-pipelines.ts
+++ bin/testing-cdk-pipelines.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { TestingCdkPipelinesStack } from '../lib/testing-cdk-pipelines-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { TestingCdkPipelinesStack } from "../lib/testing-cdk-pipelines-stack";
 
 const app = new cdk.App();
-new TestingCdkPipelinesStack(app, 'TestingCdkPipelinesStack', {
+new TestingCdkPipelinesStack(app, "TestingCdkPipelinesStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new TestingCdkPipelinesStack(app, 'TestingCdkPipelinesStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: "871477291615", region: "us-east-1" },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
